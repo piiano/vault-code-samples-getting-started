425fda5856f9437d7e8a9f338155f0fa273c6d49
Yossi Lavi <yossi.lavi@piiano.com>
2022-11-16 09:32:44 +0200

    cr fixes

--- java/prepare.sh
+++ java/prepare.sh
@@ -2,19 +2,19 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Download Piiano vault open api
+# Download Piiano vault open api.
 curl -o openapi.json https://piiano.com/docs/assets/openapi.json
 
-# Generate java sdk
+# Generate java sdk.
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli:v6.1.0 generate \
     -i local/openapi.json \
     -g java \
     -o local/vault_java_sdk
 
-// Build and install the Java sdk
+# Build and install the Java sdk.
 cd vault_java_sdk
 mvn clean install
 
-// Build and install 'Getting started'
+# Build and install 'Getting started'.
 cd ..
 mvn clean install
--- java/run.sh
+++ java/run.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# Run 'Getting started'.
 java -jar ~/.m2/repository/com/piiano/piiano-vault-getting-started-java/0.0.1-SNAPSHOT/piiano-vault-getting-started-java-0.0.1-SNAPSHOT.jar
