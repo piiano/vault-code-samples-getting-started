d14135bd7beb14e4f421372263c98cc07c204041
Nir <35661734+nirhaas@users.noreply.github.com>
2022-11-20 14:23:01 +0200

    lets try

--- java/prepare.sh
+++ java/prepare.sh
@@ -6,14 +6,14 @@ IFS=$'\n\t'
 curl -o openapi.json https://piiano.com/docs/assets/openapi.json
 
 # Generate java sdk.
-docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli:v6.2.1 generate \
-    -i local/openapi.json \
+docker run --rm -v "${PWD}:/app" openapitools/openapi-generator-cli:v6.2.1 generate \
+    -i /app/openapi.json \
     -g java \
-    -o local/vault_java_sdk
+    -o /app/vault_java_sdk
 
 # Build and install the Java sdk.
 cd vault_java_sdk
-mvn -e -X -fae clean install
+mvn -X clean install
 
 # Build and install 'Getting started'.
 cd ..
