7e6b9b70cf7ab6a3fcf50fab4826db8b03dc662d
Royi Elran <royi.elran@piiano.com>
2023-02-15 09:21:52 +0200

    install manually

--- python/prepare.sh
+++ python/prepare.sh
@@ -8,14 +8,11 @@ source .venv/bin/activate
 echo "Downloading openapi file"
 curl -o openapi.yaml https://piiano.com/docs/assets/openapi.yaml
 
-echo "Downloading wheel"
-pip install wheel
+echo "Installing openapi-client"
+pip install openapi-client
 
 echo "Running openapi tools to create the SDK"
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli:v6.1.0 generate \
     -i local/openapi.yaml \
     -g python \
     -o local/vault_python_sdk
-
-echo "Installing SDK"
-cd vault_python_sdk && pip install .
